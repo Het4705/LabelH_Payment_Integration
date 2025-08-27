a5745ca4aaf5772df3a3331873ded45180fe4e5d
Het Jayeshbhai Patel <210303105289@paruluniversity.ac.in>
2025-08-27 20:18:22 +0530

    port option

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const crypto = require("crypto");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
